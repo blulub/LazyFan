8d0254891e136a82fa42821673a20d7f6f439188
Boning Lu <blu@twitter.com>
2016-07-14 17:16:18 -0400

    Hid keys

--- LazyFanSrc/src/main/java/Constants/Keys.java
+++ LazyFanSrc/src/main/java/Constants/Keys.java
@@ -2,16 +2,16 @@ package Constants;
 
 // public keys for now, will put in server as variables later
 public class Keys {
-  public static final String consumerKey = "DcBJ6Kh2eD0Sf5ARlENJHXbvu";
-  public static final String consumerSecret = "T7dTvLMIJhVPECMElcogF71k59suu0EJkiVAxDQecniAaNI97U";
-  public static final String accessToken = "752518914391810048-3p4yz2DXbVNdZkhRLttb984wbALINRh";
-  public static final String accessTokenSecret = "32L6TaeDlOQrTIxd6c7R1AYzZtGVt1U3AfKWWZ70CcQHd";
-  public static final String seatGeakClientId = "NTE2NzQzNHwxNDY4MjQ1MTcx";
+  public static final String consumerKey = System.getenv("CONSUMER_KEY");
+  public static final String consumerSecret = System.getenv("CONSUMER_SECRET");
+  public static final String accessToken = System.getenv("ACCESS_TOKEN");
+  public static final String accessTokenSecret = System.getenv("ACCESS_TOKEN_SECRET");
+  public static final String seatGeakClientId = System.getenv("SEAT_GEEK_CLIENT_ID");
   public static final String ESPNFormat = "http://espn.go.com/%s/bottomline/scores";
 
-  public static final String MYSQL_URL = "jdbc:mysql://localhost:3306/";
-  public static final String MYSQL_USERNAME = "root";
-  public static final String MYSQL_PASSWORD = "";
-  public static final String DB_NAME = "Bot";
+  public static final String MYSQL_URL = System.getenv("MYSQL_URL");
+  public static final String MYSQL_USERNAME = System.getenv("MYSQL_USERNAME");
+  public static final String MYSQL_PASSWORD = System.getenv("MYSQL_PASSWORD");
+  public static final String DB_NAME = System.getenv("DB_NAME");
 
 }
